abd1ee17ac8555c7a378a5262b02d5b923d2cf35
David Rodriguez <33043921+Drod217@users.noreply.github.com>
2018-10-25 15:03:24 -0500

    fix

---
+++ vocalocal/src/App.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <h1 @click="onClick">
+    {{message}}
+  </h1>
+</template>
+
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+export default Component({})(
+  class App extends Vue {
+    message = "Vocal";
+    onClick() {
+      this.message = "Local";
+    }
+  }
+);
+</script>
