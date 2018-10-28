7cdc7ac02c7ff3f3213182322bc83cc5c0db72d4
David Rodriguez <33043921+Drod217@users.noreply.github.com>
2018-10-28 16:41:18 -0500

    more changes, trial with vue onclicks

---
+++ vocalocal/src/Location.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <h1>
+    <button class="styled" @click="onClick" type="button">
+    {{button}}
+    </button>
+  </h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      button: "Location",
+      onClick() {
+        alert("Test");
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.styled {
+  border: 0;
+  line-height: 2.5;
+  padding: 0 20px;
+  font-size: 1rem;
+  text-align: center;
+  color: #fff;
+  text-shadow: 1px 1px 1px #000;
+  border-radius: 10px;
+  background-color: #bb8a4a;
+  background-image: linear-gradient(
+    to top left,
+    rgba(0, 0, 0, 0.2),
+    rgba(0, 0, 0, 0.2) 30%,
+    rgba(0, 0, 0, 0)
+  );
+  box-shadow: inset 2px 2px 3px rgba(255, 255, 255, 0.6),
+    inset -2px -2px 3px rgba(0, 0, 0, 0.6);
+}
+
+.styled:hover {
+  background-color: grey;
+}
+
+.styled:active {
+  box-shadow: inset -2px -2px 3px rgba(255, 255, 255, 0.6),
+    inset 2px 2px 3px rgba(0, 0, 0, 0.6);
+}
+</style>
